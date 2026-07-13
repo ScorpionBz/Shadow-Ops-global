9178c4893092fff132877ee8a3462046be368835
ScorpionBz <scorpbz.oficial@gmail.com>
2026-07-13 00:17:12 -0600

    Add database.js with club information

---
+++ data/database.js
@@ -0,0 +1,70 @@
+const CLUBS={
+
+
+"Real Madrid":{
+
+league:"LaLiga",
+
+budget:180000000,
+
+president:"Florentino Pérez"
+
+},
+
+
+"FC Barcelona":{
+
+league:"LaLiga",
+
+budget:120000000,
+
+president:"Joan Laporta"
+
+},
+
+
+"Manchester City":{
+
+league:"Premier League",
+
+budget:200000000,
+
+president:"Sheikh Mansour"
+
+},
+
+
+"Liverpool":{
+
+league:"Premier League",
+
+budget:150000000,
+
+president:"Fenway Sports Group"
+
+},
+
+
+"Bayern Munich":{
+
+league:"Bundesliga",
+
+budget:140000000,
+
+president:"Herbert Hainer"
+
+},
+
+
+"PSG":{
+
+league:"Ligue 1",
+
+budget:170000000,
+
+president:"Nasser Al-Khelaifi"
+
+}
+
+
+};
